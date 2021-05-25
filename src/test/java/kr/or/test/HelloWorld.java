c4c9119c89d658016987bc1a61bac144da6d405d
user <user@DESKTOP-4PLECID>
2021-05-25 17:40:36 +0900

    8교시 커밋

---
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package kr.or.test;
+
+import java.io.PrintStream;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// main 자바프로그램의 진입 메서드(자동실행) 입니다.
+		System.out.println("헬로 자바!"); //시스템 출력 인쇄
+	}
+}
