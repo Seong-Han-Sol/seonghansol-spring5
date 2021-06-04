28945c73e44e4cf9f9047095bc48424a3a1a9665
user <user@DESKTOP-4PLECID>
2021-06-04 17:41:30 +0900

    8교시 커밋

--- DATABASES/SQL진도_20210603.sql
+++ DATABASES/SQL진도_20210603.sql
@@ -37,7 +37,14 @@ SELECT * FROM emp WHERE comm IS NULL;
 SELECT nvl2(comm,100,0), E.* FROM emp E WHERE NVL(comm,0) = 0;
 -- NVL2(필드명,널이아닐때100,널일때0),NVL(필드명,널일때0)
 -- 오라클은 표준쿼리X, ANSI쿼리가 표준O 입니다.
-SELECT DECODE(comm,null,0,100),NVL2(comm,100,0), E.* FROM emp E WHERE NVL(comm,0) = 0;
+SELECT 
+CASE WHEN comm is null THEN 0 
+WHEN comm = 0 THEN 100
+WHEN comm > 0 THEN comm
+END AS "CASE출력문"
+,DECODE(comm,null,0,100)
+,NVL2(comm,100,0)
+,E.* FROM emp E;-- WHERE NVL(comm,0) = 0;
 -- 연봉기준으로 정렬 sort = 순서 order by 필드명 오름차순[초기값]|내림차순
 --(중)서브쿼리?(select쿼리가 중복되어있는...) 입니다.
 SELECT ROWNUM, E.* FROM (--테이블명
@@ -53,17 +60,18 @@ SELECT DISTINCT deptno AS "부서번호" FROM emp;
 SELECT ename ||' is a '|| job AS "문자열 연결" FROM emp;
 -- 여기까지 select 마무리 Read
 -- 이후에는 CRUD중에 Insert, Update, Delete 3개의 쿼리로 끝
--- 함수용어 ABS (Absolute절대값), Floor(바닥함수 1.5=1)<->Ceil(천장함수 1.5=2)
--- ROUND(반올림), TRUNC(Truncate버리는 함수), Mod(나머지 구하는 함수)
--- Upper(영어 대문자로), Lower(영어 소문자로), Length(길이구하는 함수)
--- Instr(문자의 위치를 구하는 함수), Substr(매개변수로 입력한 숫자위치만큼의 문자 추출)
--- Lpad(LeftPadding 왼쪽여백), Rpad(오른쪽여백), 레포트프로그램에서 출력조정시 사용
--- Trim(왼쪽, 오른쪽 문자열을 잘라내는 함수) 
--- 날짜 sysdate로 오라클전용 함수로서 게시물입력시간, 회원등록 시간
-SELECT to_char(systimestamp, 'yyyy-mm-dd hh24:mi:ss:ff') sysdate FROM dual;
+-- 함수용어 ABS(Absolute절대값), Floor(바닥함수1.5=1)<->Ceil(천정함수1.5=2)
+-- ROUND(반올림), TRUNC(Truncate버리는함수), Mod(나머지구하는함수)
+-- Upper(대문자),Lower(소문자),Length(길이구하는함수)
+-- Instr(문자의위치를구하는함수), Substr(매개변수로 입력한 숫자위치만큼 문자열을 추출하는함수)
+-- Lpad(LeftPadding왼쪽여백), Rpad(오른쪽여백), 레포트프로그램에서 출력조정시
+-- Trim(왼쪽,오른쪽 문자열을 잘라내는 함수)
+-- 날짜 sysdate,systimestamp 로 오라클전용 함수로서 게시물입력시간, 회원등록 시간
+SELECT to_char(systimestamp,'yyyy-mm-dd hh24:mi:ss:ff') FROM dual;
+-- 위 to_char(날짜를 문자열로 변환)형 변환함수라고 합니다.
 SELECT sysdate + 1 FROM dual;
 SELECT sysdate - 1 FROM dual;
 --아래용도는 6개월간 회원정보 수정이 없는 회원에게 공지서비스를 처리
-SELECT * FROM
-TBL_MEMBER
-WHERE UPDATE_DATE < ADD_MONTHS
+SELECT * FROM 
+TBL_MEMBER 
+WHERE UPDATE_DATE < ADD_MONTHS(SYSDATE,-6);
