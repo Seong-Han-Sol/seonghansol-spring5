abf8cdee1437b760b79bc9952469ce2136ad7b07
User <User@DESKTOP-QLDLAUR>
2021-06-07 11:15:12 +0900

    3교시 중간 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -39,7 +39,7 @@ public class DataSourceTest {
 	public void oldQueryTest() throws Exception {
 		//스프링빈을 사용하지 않을때 예전 방식: 코딩테스트에서는 스프링설정을 안쓰고, 직접 DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE2","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공 하였습니다. DB종류는 "+ connection.getMetaData().getDatabaseProductName());
 		//직접쿼리를 날립니다. 날리기전 쿼리문장 객체생성statement
 		Statement stmt = connection.createStatement();
@@ -53,7 +53,7 @@ public class DataSourceTest {
 		 */
 		//인서트,업데이트,삭제시 sql디벨러퍼에서는 커밋이 필수지만, 외부java클래스에서는 자동커밋이 됩니다.
 		//테이블에 입력되어 있는 레코드셋를 select 쿼리 stmt문장으로 가져옴(아래)
-		ResultSet rs = stmt.executeQuery("select * from dept02 order by deptno");//20년전 작업방식
+		ResultSet rs = stmt.executeQuery("select * from dept order by deptno");//20년전 작업방식
 		//위에서 저장된 rs객체를 반복문으로 출력(아래)
 		while(rs.next()) {
 			//rs객체의 레코드가 없을때까지 반복
