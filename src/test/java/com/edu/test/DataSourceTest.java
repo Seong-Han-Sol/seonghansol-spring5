7d2afc6c4936e3d3885150397a8dbe5e6ab461be
user <user@DESKTOP-4PLECID>
2021-06-08 19:35:27 +0900

    8교시 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -16,10 +17,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.edu.service.IF_MemberService;
+import com.edu.vo.MemberVO;
+
 /**
  * 이 클래스는 오라클과 연동해서 CRUD를 테스트하는 클래스 입니다.
  * 과장(이사,팀장) JUnit CRUD까지 만들어서 일반사원에게 공개 + 회원관리
- * @author 김일국
+ * @author 성한솔
  *
  */
 //RunWith인터페이스 현재클래스가 Junit실행클래스라고 명시
@@ -32,24 +36,33 @@ public class DataSourceTest {
 	private Logger logger = Logger.getLogger(DataSourceTest.class);
 	//dataSource 객체는 데이터베이스객체를 pool로 저장해서 사용할때 DataSource 클래스를 사용(아래)
 	@Inject //인젝트는 스프링에서 객체를 만드는 방법, 이전 자바에서는 new 키워드로 객체를 만들었고... 
-	DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
+	private DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
 	//Inject 자바8부터 지원, 그럼, 이전 자바7에서 @Autowired 로 객체를 만들었슴
+	@Inject //MemberService서비스를 주입받아서 객체를 사용합니다.(아래)
+	private IF_MemberService memberService;
 	
 	//스프링 코딩 시작 순서
-	// M-V-C 사이에 데이터를 임시 저장하는 공간(VO클래스-멤버변수+GET/Set메서드) 생성
-	// 보통 ValueObject클래스는 DB테이블과 1:1로 매칭이 됩니다.
-	//그래서, 1. MemberVO.java VO클래스를 생성. (필수)
-	//2. DB(마이바티스)쿼리를 만듭니다. (VO사용됨)
+		// M-V-C 사이에 데이터를 임시 저장하는 공간(VO클래스-멤버변수+GET/Set메서드) 생성
+		// 보통 ValueObject클래스는 DB테이블과 1:1로 매칭이 됩니다.
+		//그래서, 1. MemberVO.java VO클래스를 생성. (필수)
+		//2. DB(마이바티스)쿼리를 만듭니다. (VO사용됨)
 	@Test
 	public void selectMember() throws Exception {
-		//회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력하는 메서드 테스트 ->회원관리목록이 출력
+		//회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력 메서드 테스트->회원관리목록이 출력
+		//현재100명 검색기능, 페이징기능 여기서 구현. 1페이지에 10명씩 나오게 변경
+		//현재 몇페이지, 검색어 임시저장 공간 -> DB에 페이징 조건문, 검색 조건문
+		//변수를 2-3이상은 바로 String변수로 처리하지 않고, VO만들어 사용.
+		//PageVO.java 페이징처리변수와 검색어 변수 선언, Get/Set생성
+		//PageVO 만들기 전 SQL쿼리로 가상으로 페이지를 한번 구현해 보면서, 필요한 변수를 만들어야 한다.
+		List<MemberVO> listMember = memberService.selectMember();
+		listMember.toString();
 	}
 	
 	@Test
 	public void oldQueryTest() throws Exception {
 		//스프링빈을 사용하지 않을때 예전 방식: 코딩테스트에서는 스프링설정을 안쓰고, 직접 DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE2","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공 하였습니다. DB종류는 "+ connection.getMetaData().getDatabaseProductName());
 		//직접쿼리를 날립니다. 날리기전 쿼리문장 객체생성statement
 		Statement stmt = connection.createStatement();
