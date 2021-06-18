78a7cca733cd9086fd87c5edd72aa44d6885405d
user <user@DESKTOP-4PLECID>
2021-06-18 14:06:05 +0900

    4교시 커밋

---
+++ src/main/java/com/edu/service/BoardServiceImpl.java
@@ -0,0 +1,65 @@
+package com.edu.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.edu.dao.IF_BoardDAO;
+import com.edu.vo.AttachVO;
+import com.edu.vo.BoardVO;
+import com.edu.vo.PageVO;
+
+/**
+ * 이 클래스는 DAO메서드를 호출하는 기능을 합니다.
+ * @author 성한솔
+ *
+ */
+@Service //@애노테이션을 붙이면 스프링 빈으로 등록이 됨.
+public class BoardServiceImpl implements IF_BoardService {
+	@Inject
+	private IF_BoardDAO boardDAO;
+	@Override
+	public List<AttachVO> readAttach(Integer bno) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int countBoard(PageVO pageVO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deleteBoard(int bno) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBoard(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BoardVO readBoard(int bno) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertBoard(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<BoardVO> selectBoard(PageVO pageVO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
