07added15492a0a31f543c5c249fd69fea42860e
Seong-Han-Sol <seoryeong1222@gmail.com>
2021-07-08 14:09:09 +0900

    4교시 커밋

--- src/main/java/com/edu/service/IF_MemberService.java
+++ src/main/java/com/edu/service/IF_MemberService.java
@@ -12,6 +12,7 @@ import com.edu.vo.PageVO;
  *
  */
 public interface IF_MemberService {
+	
 	public List<MemberVO> selectMember(PageVO pageVO) throws Exception;
 	//이곳에 CRUD 메서드가 계속 추가됩니다.
 	public int countMember(PageVO pageVO) throws Exception;
